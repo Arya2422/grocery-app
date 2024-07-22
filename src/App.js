8db915e1ad3e48fbdbc5602cdf5f3e472a6c6808
Arya Sali <123355820+Arya2422@users.noreply.github.com>
2024-07-22 17:30:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,34 +15,31 @@ import UserProfile from './Pages/User/UserProfile';
 import FAQ from './Pages/Extra/FAQ';
 import Termsandconditions from './Pages/Extra/Termsandconditions';
 import PrivacyPolicy from './Pages/Extra/PrivacyPolicy';
+import { RecoilRoot } from 'recoil';
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/product/:prodid" 
-          element={
-            <ProductPage/>
-          }
-        />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/forgotpassword" element={<ForgotPassword/>} />
-        <Route path="/cart" element={<Cart/>} />
-        <Route path='/user/:activepage' element={<UserProfile/>} />
-        <Route path="/FAQ" element={<FAQ/>} />
-        <Route path="/termsandconditions" element={<Termsandconditions/>} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy/>} />
-
-        <Route path="*" element={<div>
-          <h1>404 NOT FOUND</h1>
-        </div>} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/product/:prodid" element={<ProductPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path='/user/:activepage' element={<UserProfile />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="/termsandconditions" element={<Termsandconditions />} />
+          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="*" element={<div><h1>404 NOT FOUND</h1></div>} />
+        </Routes>
+      </RecoilRoot>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
